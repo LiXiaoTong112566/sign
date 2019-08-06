b62b92681244d567655ef44139a1fa381d144e11
hmm <HMM5412@163.com>
2019-08-06 20:31:43 +0800

    页面

--- sign/src/pages/counter/index.vue
+++ sign/src/pages/counter/index.vue
@@ -1,10 +1,11 @@
 <template>
   <div class="counter-warp">
-    <p>Vuex counter：{{ count }}</p>
+    <!-- <p>Vuex counter：{{ count }}</p>
     <p>
       <button @click="increment">+</button>
       <button @click="decrement">-</button>
-    </p>
+    </p> -->
+    
   </div>
 </template>
 
@@ -14,31 +15,21 @@
 
 export default {
   computed: {
-    count () {
-      return store.state.count
-    }
+    // count () {
+    //   return store.state.count
+    // }
   },
   methods: {
-    increment () {
-      store.commit('increment')
-    },
-    decrement () {
-      store.commit('decrement')
-    }
+    // increment () {
+    //   store.commit('increment')
+    // },
+    // decrement () {
+    //   store.commit('decrement')
+    // }
   }
 }
 </script>
 
 <style>
-.counter-warp {
-  text-align: center;
-  margin-top: 100px;
-}
-.home {
-  display: inline-block;
-  margin: 100px auto;
-  padding: 5px 10px;
-  color: blue;
-  border: 1px solid blue;
-}
+
 </style>
---
+++ sign/src/pages/listForm/index.vue
@@ -0,0 +1,44 @@
+<template>
+    <div class="formList">
+       <ul>
+           <li>
+               <span>面试地址：</span>
+               <input type="text" value="北京市海淀区上地软件园南路57号">
+           </li>
+       </ul>
+       <div>
+           <button>去打卡</button>
+           <button>放弃面试</button>
+       </div>
+    </div>
+</template>
+<script>
+export default {
+    props:{
+
+    },
+    components:{
+
+    },
+    data(){
+        return {
+
+        }
+    },
+    computed:{
+
+    },
+    methods:{
+
+    },
+    created(){
+
+    },
+    mounted(){
+
+    }
+}
+</script>
+<style scoped lang="">
+
+</style>
