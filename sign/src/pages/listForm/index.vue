e2c4d75431a5c461aa0fad98b9e5047755996814
hmm <HMM5412@163.com>
2019-08-06 21:34:05 +0800

    增加客服排版

--- sign/src/pages/listForm/index.vue
+++ sign/src/pages/listForm/index.vue
@@ -5,24 +5,44 @@
                <span>面试地址：</span>
                <input type="text" value="北京市海淀区上地软件园南路57号">
            </li>
+           <li>
+               <span>面试时间：</span>
+               <input type="text" value="2019-08-06 17:00">
+           </li>
+           <li>
+               <span>联系方式：</span>
+               <input type="text" value="15532568521">
+           </li>
+           <li>
+               <span>是否提醒：</span>
+               <div>未提醒</div>
+           </li>
+           <li>
+               <span>面试状态：</span>
+               <div>未开启</div>
+           </li>
+           <li>
+               <span>取消提醒：</span>
+               
+           </li>
        </ul>
-       <div>
-           <button>去打卡</button>
-           <button>放弃面试</button>
+       <div class="btns">
+           <button style="background:blue">去打卡</button>
+           <button style="background:#BE0000">放弃面试</button>
        </div>
     </div>
 </template>
 <script>
 export default {
     props:{
-
+       
     },
     components:{
 
     },
     data(){
         return {
-
+           
         }
     },
     computed:{
@@ -39,6 +59,39 @@ export default {
     }
 }
 </script>
-<style scoped lang="">
-
+<style scoped >
+.formList{
+    width:100%;
+    box-sizing: border-box;
+    padding:0 10px;
+}
+.formList ul li{
+  width:100%;
+  height:40px;
+  box-sizing: border-box;
+  padding:8px 0;
+  border-bottom: 1px solid #eee;
+  display:flex;
+  font-size: 15px;
+}
+li span{
+    color:#ccc;
+    padding-left:5px;
+}
+.address input{
+  height:100%;
+  margin-left:10px;
+}
+.btns{
+    display:flex;
+    justify-content: space-between;
+}
+.btns button{
+    display:inline-block;
+    width:150px;
+    color:#fff;
+    border-radius: 0;
+    border:0;
+    margin-top:50px;
+}
 </style>
